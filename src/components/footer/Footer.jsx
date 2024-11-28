5196a6bdf9e91827ef81ceb846265d3135f62432
Lucas <lucas.lima3@celsoonline.com.br>
2024-11-27 21:04:00 -0300

    finalizado pagina doação

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,25 @@
 import s from './footer.module.scss';
+import facebookicon from '../../assets/facebookicon.png';
+import twittericon from '../../assets/twittericon.png';
+import youtubeicon from '../../assets/youtubeicon.png';
+import linkedinicon from '../../assets/linkedinicon.png';
+import instagramicon from '../../assets/instagramicon.png';
 export default function Footer() {
     return (
       <footer className={s.footer}>
-        <h2>4002 8922</h2>
+          <section className={s.redes}>
+            <p>4004-8921</p>
+            <nav>
+              <a href="">< img src={facebookicon} alt='icone do Facebook' /></a>
+              <a href="">< img src={twittericon} alt='icone do Twitter' /></a>
+              <a href="">< img src={youtubeicon} alt='icone do Youtube' /></a>
+              <a href="">< img src={linkedinicon} alt='icone do Linkedin' /></a>
+              <a href="">< img src={instagramicon} alt='icone do instagram'/></a>
+            </nav>
+          </section>
+          <section className={s.copyright}>
+              <p> Layout desenvolvido pela Vai na Web para fins educativos - 2024 </p>
+          </section>
       </footer>
     );
   }
