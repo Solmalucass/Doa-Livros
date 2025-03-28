d1fdaf67a5e343367960f39dd39584ad8287bd3f
Lucas <lucas.lima3@celsoonline.com.br>
2025-03-28 19:44:19 -0300

    api

--- src/pages/queroDoar/Querodoar.jsx
+++ src/pages/queroDoar/Querodoar.jsx
@@ -1,6 +1,44 @@
+import axios from 'axios';
 import iconLivro from '../../assets/Vector.png'
 import s from './queroDoar.module.scss'
-export default function QueroDoar() {
+import { useState } from 'react'; 
+
+export default function QueroDoar(){
+  
+  const [titulo, setTitulo] = useState("")
+  const [categoria, setCategoria] = useState("")
+  const [autor, setAutor] = useState("")
+  const [image_url, setImagem_url] = useState("")
+
+    const capturaTitulo = (e) => {
+      setTitulo(e.target.value)
+    }
+
+    const capturaCategoria = (e) => {
+      setCategoria(e.target.value)
+    }
+
+    const capturaAutor = (e) => {
+      setAutor(e.target.value)
+    }
+
+    const capturaImagem = (e) => {
+      setImagem_url(e.target.value)
+    }
+
+    const EnvioDados = async() => {
+
+      const dadosEnviar = {
+        titulo,
+        categoria,
+        autor,
+        image_url
+    }
+  
+
+      await axios.post("https://flask-api-htrv.onrender.com/doa-livros", dadosEnviar)
+    }
+
   return (
     <section className={s.sectionQueroDoar}>
       <p>Por favor, preencha o formulário com suas informações e as informações do Livro.</p>
@@ -9,11 +47,11 @@ export default function QueroDoar() {
           <img src={iconLivro} alt="icone livro aberto" />
           <h2>Informações do Livro</h2>
         </div>
-        <input type="text" name="" id="" placeholder="Título" />
-        <input type="text" name="" id="" placeholder="Categoria" />
-        <input type="text" name="" id="" placeholder="Autor" />
-        <input type="text" name="" id="" placeholder="Link da Imagem" />
-        <input submite type="submit" value="Doar" className={s.buttonDoar} />
+        <input type="text" name="" id="" placeholder="Título" onChange={capturaTitulo}/>
+        <input type="text" name="" id="" placeholder="Categoria" onChange={capturaCategoria}/>
+        <input type="text" name="" id="" placeholder="Autor" onChange={capturaAutor}/>
+        <input type="text" name="" id="" placeholder="Link da Imagem"onChange={capturaImagem} />
+        <input submite type="submit" value="Doar" className={s.buttonDoar} onClick={EnvioDados}/>
 
       </form>
     </section>
