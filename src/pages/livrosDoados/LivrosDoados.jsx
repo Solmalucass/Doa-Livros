7a2ee9c0fc42cb7cc387fd87dd49e8a36f221d18
Lucas <lucas.lima3@celsoonline.com.br>
2025-04-02 18:35:14 -0300

    terekl

--- src/pages/livrosDoados/LivrosDoados.jsx
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -1,6 +1,21 @@
 import oProtagonista from "../../assets/protagonista.png";
 import s from "./livrosDoados.module.scss";
+import { useEffect, useState } from "react";
+import axios from "axios"
+
 export default function LivrosDoados() {
+
+  const [livros, setLivros] = useState([]);
+
+  const puxarLivros = async () => {
+    const resposta = await axios.get("https://apivainolivro.onrender.com/livros-doados");
+    setLivros(resposta.data)
+  }
+
+  useEffect(() => {
+    puxarLivros();
+  }, []);
+
   return (
     <section className={s.livrosDoadosSection}>
       <h2>Livros Doados</h2>
